ddecb4b6c0c38d733f5ac200025687516795743c
Zach Pomerantz <zmp@umich.edu>
2015-04-26 19:57:23 +0000

    Use correct /usr/src/kernel dir

--- update.sh
+++ update.sh
@@ -19,11 +19,11 @@ setup() {
 	# Fetch ChromeOS kernel sources
 	which git || apt-get install git
 	cd /usr/src
-	git clone --depth 1 --single-branch --branch chromeos-${kernel_version:0:4} https://chromium.googlesource.com/chromiumos/third_party/kernel src
+	git clone --depth 1 --single-branch --branch chromeos-${kernel_version:0:4} https://chromium.googlesource.com/chromiumos/third_party/kernel
 }
 
 build_kernel() {
-	cd src
+	cd /usr/src/kernel
 
 	# Build Ubuntu kernel packages
 	which make-kpkg || apt-get install kernel-package
@@ -35,7 +35,8 @@ build_kernel() {
 
 install_kernel() {
 	KERNEL_DEBS=$1
-	cd src
+	DIR=`pwd`
+	cd /usr/src/kernel
 
 	# Create a kernel signing key
 	mkdir -p /usr/share/vboot/devkeys
@@ -51,33 +52,33 @@ install_kernel() {
 
 	# Backup current kernel
 	tstamp=$(date +%Y-%m-%d-%H%M)
-	dd if=/dev/mmcblk0p6 of=../kernel-backup-$tstamp
+	dd if=/dev/mmcblk0p6 of=$DIR/kernel-backup-$tstamp
 	cp -Rp /lib/modules/`uname -r` /lib/modules/`uname -r`-backup-$tstamp
 
 	# Install built kernel image/modules
 	dpkg -i $KERNEL_DEBS
 
 	# Extract old kernel config
-	vbutil_kernel --verify /dev/mmcblk0p6 --verbose | tail -1 > ../config-$tstamp-orig.txt
+	vbutil_kernel --verify /dev/mmcblk0p6 --verbose | tail -1 > $DIR/config-$tstamp-orig.txt
 
 	# Add permissions to a new config
 	sed -e 's/$/ disablevmx=off/' \
-		../config-$tstamp-orig.txt > ../config-$tstamp.txt
+		$DIR/config-$tstamp-orig.txt > $DIR/config-$tstamp.txt
 
 	# Repack the new kernel
-	vbutil_kernel --pack ../newkernel \
+	vbutil_kernel --pack $DIR/repacked_kernel \
 		--keyblock /usr/share/vboot/devkeys/kernel.keyblock \
 		--version 1 \
 		--signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk \
-		--config=../config-$tstamp.txt \
+		--config=$DIR/config-$tstamp.txt \
 		--vmlinuz /boot/vmlinuz-`uname -r` \
 		--arch x86_64
 
 	# Verify the new kernel
-	vbutil_kernel --verify /newkernel
+	vbutil_kernel --verify $DIR/repacked_kernel
 
 	# Copy the new kernel to the ChrUbuntu partition
-	dd if=../newkernel of=/dev/mmcblk0p6
+	dd if=$DIR/repacked_kernel of=/dev/mmcblk0p6
 
 	cd -
 }
